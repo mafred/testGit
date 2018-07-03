c384807bb91894ccebfc8b39a6cf325419c9d879
ghzhu3 <ghzhu3@iflytek.com>
2018-07-03 19:17:01 +0800

    web项目上传

---
+++ src/com/iflytek/test.java
@@ -0,0 +1,10 @@
+package com.iflytek;
+
+/**
+ * Created by ghzhu3 on 2018/7/3.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
