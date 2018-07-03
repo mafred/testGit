358541942545fe5f0081bc4cd884a043631154e6
mafred <40712710+mafred@users.noreply.github.com>
2018-07-03 19:39:07 +0800

    Update test.java
    12

--- src/com/iflytek/test.java
+++ src/com/iflytek/test.java
@@ -6,7 +6,7 @@ package com.iflytek;
 public class test {
     public static void main(String[] args) {
         System.out.println("version 2");
-        System.out.println("version 2");
+        System.out.println("version 12");
         System.out.println("version 3");
     }
 }
